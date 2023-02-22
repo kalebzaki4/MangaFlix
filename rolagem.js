35a3fa813ec6e6d072e4d359d156e779441f34da
Kaleb Zufanetti Santos <kalebzsantosk@gmail.com>
2023-02-22 00:04:16 -0300

    Create rolagem.js

---
+++ rolagem.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+});
+
+const hiddenElements = document.querySelectorAll('.abrir')
+hiddenElements.forEach((el) => observer.observe(el))
