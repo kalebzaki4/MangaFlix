41fb51162d6617e8a3d6033c659e0ebbfbaaf015
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-05-25 11:05:50 -0300

    Create scroll-animation.js

---
+++ assets/js/scroll-animation.js
@@ -0,0 +1,19 @@
+const scrollAnima = document.querySelector('[data-anima="scroll"]');
+
+const metadeWindow = window.innerHeight * 1.5
+
+function animarScroll() {
+    const topoItem = scrollAnima.getBoundingClientRect().top;
+
+    const itemVisivel = topoItem - metadeWindow < 0 
+
+    if(itemVisivel) {
+        scrollAnima.classList.add('show-button');
+    } else {
+        scrollAnima.classList.remove('show-button');
+    };
+};
+
+
+
+window.addEventListener('scroll', animarScroll);
