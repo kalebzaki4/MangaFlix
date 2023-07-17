308ba40d19e13af9a413c43cdcf047c936371002
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-07-17 18:07:25 -0300

    app.css e app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Menu from './components/Menu'
+import './App.css';
+import Menu from './components/Menu/Menu';
 
 function App() {
   return (
-    <>
-      <div className='App'>
-        <Menu />
-      </div>
-    </>
-  )
+    <Menu />
+  );
 }
 
-export default App
+export default App;
