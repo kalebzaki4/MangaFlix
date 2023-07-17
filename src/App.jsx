02e09fb99926d70caf91734aa58bc878ff8e1983
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-07-17 16:46:21 -0300

    usando o react e importando o menu

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Menu from './components/Menu'
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <Menu />
+      </div>
+    </>
+  )
+}
+
+export default App
