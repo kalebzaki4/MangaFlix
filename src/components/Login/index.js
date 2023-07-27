61850003814eb026f69197bd2a4a9b41575ccf1e
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-07-26 21:54:34 -0300

    novo componentes da tela de login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -10,29 +10,27 @@ const Login = (props) => {
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        props.aoColaboradorCadastrado({
-            email,
-            senha
-        })
+        console.log('form submetido =>', email, senha)
     }
 
     return (
         <section>
+            <div className='img'>
+                <img className='imagem' src="./Mangaflix.png" width='500px' alt="" />
+            </div>
             <form onSubmit={aoSalvar}>
-                <div class="form-box">
+                <div class="form-box gradient-border">
                     <div class="form-value">
-                        <form action="">
-                            <h2>Conecte-se</h2> 
-                            <CampoTexto obrigatorio={true} label="Email" valor={email} aoAlterado={valor => setEmail(valor)} />
-                            <CampoTexto obrigatorio={true} label="Senha" valor={senha} aoAlterado={valor => setSenha(valor)} />
-                            <div class="forget">
-                                <label for=""><input type="checkbox"/>Lembre de mim  <a href="#">Esqueceu a senha</a></label>
-                            </div>
-                            <Botao>Entrar</Botao>
-                            <div class="register">
-                                <p>não tem uma conta <a href="#">Registro</a></p>
-                            </div>
-                        </form>
+                        <h2>Conecte-se</h2>
+                        <CampoTexto obrigatorio={true} label="Email" valor={email} aoAlterado={valor => setEmail(valor)} />
+                        <CampoTexto obrigatorio={true} label="Senha" valor={senha} aoAlterado={valor => setSenha(valor)} />
+                        <div class="forget">
+                            <label for=""><input type="checkbox" />Lembre de mim  <a href="#">Esqueceu a senha</a></label>
+                        </div>
+                        <Botao>Entrar</Botao>
+                        <div class="register">
+                            <p>não tem uma conta <a href="#">Registro</a></p>
+                        </div>
                     </div>
                 </div>
             </form>
