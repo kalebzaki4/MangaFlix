70ff41ad53f120601b97a651c6434fae045ccabd
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-08-10 11:20:28 -0300

    componente app terminado

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './App.css';
+import Login from './components/Login';
+
+function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+  }
+
+  return (
+    <div className="App">
+      <Login  aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+    </div>
+  );
+}
+
+export default App;
