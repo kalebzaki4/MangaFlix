553fece69cdd5db2699d53f9edc167655d25d3f5
Kaleb Zufanetti Santos <109079500+kalebzaki4@users.noreply.github.com>
2023-08-12 18:29:48 -0300

    pagina de login no app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import Menu from './components/Menu/Menu';
+
+function App() {
+  return (
+    <Menu />
+  );
+}
+
+export default App;
---
+++ src/componentes/Menu/index.jsx
@@ -0,0 +1,15 @@
+import './Menu.css'
+
+const Menu = (props) => {
+    return (
+        <nav>
+            <div className='nav-bar'>
+            <i class='bx bx-menu sidebarOpen' ></i>
+            <span class="logo"><a href="index.html">
+            <img src="/images/banner.png" alt="O banner principal da página do Organo"/></a></span>
+
+            </div>
+        </nav> )
+}
+
+export default Menu
